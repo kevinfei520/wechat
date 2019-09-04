44f733b5cc6b6be79ae4296d5a06d94e78a70671
jingfeiMac <794783766@qq.com>
2019-09-04 12:39:41 +0800

    sdasd

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 
-$route['404_override'] = '';
+$route['404_override'] = 'errors/page_missing';
 
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -32,7 +32,7 @@ class Welcome extends CI_Controller {
 			'token'          => 'WechatToken',
 			'appid'          => 'wxf424cee783ba41b8',
 			'appsecret'      => '9a3b805dd87ab76c53edd79f5fb937ad',
-			'encodingaeskey' => '',
+			'encodingaeskey' => 'XsGiOQzKJX6nLnoObNmgcdH3yB2BW2scAJIyh4YxSJ4',
 			'type'			 => 'user',
 		);
 		
@@ -40,26 +40,39 @@ class Welcome extends CI_Controller {
 		//文件夹名注意大写
 		$this->load->library('Wechat/wechat_user', $config);
 		
-		var_dump($this->wechat_user->getUserList());
+		// var_dump($this->wechat_user->getUserList());
 	}
 	
 	public function index(){
-		/*
-		# 配置参数
-		$config = array(
-			'token'          => 'openant',
-			'appid'          => 'wxfc790e2eb9601add',
-			'appsecret'      => '39ea2b90c55ec14462b1967909316895',
-			'encodingaeskey' => '',
-		);
 
-		# 加载对应操作接口
-		$this->wechat->get('User', $config);
-		//$userlist = $wechat->getUserList();
+		/*
+			# 配置参数
+			$config = array(
+				'token'          => 'openant',
+				'appid'          => 'wxfc790e2eb9601add',
+				'appsecret'      => '39ea2b90c55ec14462b1967909316895',
+				'encodingaeskey' => '',
+			);
+
+			# 加载对应操作接口
+			$this->wechat->get('User', $config);
+			//$userlist = $wechat->getUserList();
 		*/
 		//var_dump($userlist);
 		//var_dump($wechat->errMsg);
 		//var_dump($wechat->errCode);
 	}
 
+	public function valid()
+	{	
+		$config = array(
+			'token'          => 'WechatToken',
+			'appid'          => 'wxf424cee783ba41b8',
+			'appsecret'      => '9a3b805dd87ab76c53edd79f5fb937ad',
+			'encodingaeskey' => 'XsGiOQzKJX6nLnoObNmgcdH3yB2BW2scAJIyh4YxSJ4',
+			'type'			 => 'user',
+		);
+		$this->load->library('Wechat/lib/Wechat_common', $config);
+		var_dump($this->wechat_common->valid());
+	}
 }
