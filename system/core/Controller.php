b962b969e67ea8a11bc31dffc826f8a322695f30
jingfeiMac <794783766@qq.com>
2019-09-14 04:03:56 +0800

    dasda

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -35,7 +35,7 @@
  * @since	Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+// defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Application Controller Class
