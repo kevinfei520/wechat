5d58e65f5dca968dd2fc43939cf69b719656e23f
jingfeiMac <794783766@qq.com>
2019-09-14 04:13:42 +0800

    dasdas

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -16,8 +16,30 @@ class Welcome extends CI_Controller {
 		{
 			$this->load->library('Wechat/Wechat_oauth', self::getWconfig());
 			$url = $this->wechat_oauth->getOauthRedirect('http://weixin.kevinfei.com/welcome/getuserinfo','STATE');
-			file_get_contents($url);
+			self::http_get($url);
 		}
 	}
 
+	/**
+     * GET 请求
+     * @param string $url
+     */
+    static public function http_get($url) {
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+    }
 }
