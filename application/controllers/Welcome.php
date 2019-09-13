fe3b1333c0e9ca716773ffad39e5c7a00e20caef
jingfeiMac <794783766@qq.com>
2019-09-14 04:09:50 +0800

    dasd

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,21 +1,42 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Weixin Authentication
  */
 class Welcome extends CI_Controller {
 
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
 	public function index()
 	{	
-		$this->load->library('Wechat/Wechat_oauth', self::getWconfig());
-		$url = $this->wechat_oauth->getOauthRedirect('http://weixin.kevinfei.com/welcome/getuserinfo','STATE');
-		file_get_contents($url);
 	}
 
 	public function getUserInfo()
 	{	
-		$this->index();
-		var_dump($_GET['code']);
+		if( $_GET['code'] )
+		{
+			var_dump( $_GET['code'] );
+		}
+		else
+		{
+			$this->load->library('Wechat/Wechat_oauth', self::getWconfig());
+			$url = $this->wechat_oauth->getOauthRedirect('http://weixin.kevinfei.com/welcome/getuserinfo','STATE');
+			file_get_contents($url);
+		}
 	}
 
 }
