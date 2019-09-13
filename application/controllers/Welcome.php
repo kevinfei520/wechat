40ef14f0a7475b628e1ce28372c05f46db2abcc1
jingfeiMac <794783766@qq.com>
2019-09-14 03:59:56 +0800

    dasd

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,21 +25,12 @@ class Welcome extends CI_Controller {
 	{	
 		$this->load->library('Wechat/Wechat_oauth', self::getWconfig());
 		$url = $this->wechat_oauth->getOauthRedirect('http://weixin.kevinfei.com/welcome/getuserinfo','STATE');
-		var_dump( file_get_contents($url) );die;
-
+		echo file_get_contents($url); die;
 	}
 
 	public function getUserInfo()
 	{
-		var_dump($_GET['code']);
+		echo $_GET['code'];
 	}
-
-	/**
-     * GET 请求
-     * @param string $url
-     */
-    public function http_get($url) {
-        
-    }
 
 }
