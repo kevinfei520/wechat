0c38f3651b530313d76859a89d57d141b860d5fe
jingfeiMac <794783766@qq.com>
2019-09-14 00:39:26 +0800

    dsadas

--- application/controllers/Weixinvaild.php
+++ application/controllers/Weixinvaild.php
@@ -20,17 +20,5 @@ class Weixinvaild extends CI_Controller {
 		$this->load->library('Wechat/Wechat_receive', self::getWconfig());
 		echo $this->wechat_receive->text('hello')->reply();
 	}
-	
-	/**
-	 * [autoReply  自动回复消息]
-	 * @author   jingfeiMac  794783766@qq.com
-	 * @datetime 2019-09-12T16:03:55+0800
-	 * @return   [type]                   [description]
-	 */
-	public function index()
-	{	
-		// $this->load->library('Wechat/Wechat_receive', self::getWconfig());
-		// echo $this->wechat_receive->text('msg')->reply();
-	}
 
 }
